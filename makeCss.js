c8a8159b909968154d711cee8b8defbb80251218
Rohit Sehgal <Rohit@varuns-iphone.local>
2015-03-12 00:25:20 -0500

    butt

--- makeCss.js
+++ makeCss.js
@@ -1,27 +1,58 @@
 var cssArray = [];
-var createCss = function(number, increment, maxInt){
-     var x = 0;
-     var cool = "";
+var allArray = [];
+var createCss = function(number, increment){
+    var x = 0;
+    var x1 = 0;
+     var n = 0;
+     var z = 0;
+     var stringToReplaceNumber = 0;
+     var stringToReplace = ""
+     var maxIntArray = [];
+
+     if(number < 10){
+       maxIntDecimal = 0.1;
+     }else if(number > 99 && number > 999){
+       maxIntDecimal = 0.01;
+     }else if(number > 999 && number > 9999){
+       maxIntDecimal = 0.001;
+     }else if(number > 9999 && number > 99999){
+       maxIntDecimal = 0.0001;
+     }else if(number > 99999 && number > 999999){
+       maxIntDecimal = 0.00001;
+     }else if(number > 999999 && number > 9999999){
+       maxIntDecimal = 0.000001;
+     }
+     for(var k = 0; k < number; k++){
+       x1 += increment;
+     }
+     var cool = "{ .h1{ font-size: 60px; }; } "
+     for(var j = 0; j < cool.length; j++){
+       n = cool.indexOf("px");
+     }
+       if(n != null){
+         for(var l = 0; l < number; l++){
+           console.log(l.toString())
+           stringToReplaceNumber = number - l;
+           console.log(stringToReplaceNumber)
+           stringToReplace = number.toString() + "px";
+           var re = new RegExp(stringToReplace, 'g');
+           var replacedString = cool.replace(re, l.toString() + "px");
+           console.log(replacedString);
+           maxIntArray.push(replacedString)
+         }
+       }
         for(var i = 0; i < number; i++){
             x+= increment;
-            cool = "@media(max-width: 60px) { .h1{ font-size: 60px; }}";
-            console.log(cool);
+            cool = "@media(min-width: " + x + "px)";
             cssArray.push(cool);
+
         };
-        var n = 0;
-        var z = 0;
-        for(var j = 0; j < cool.length; j++){
-          n = cool.indexOf("px");
+
+        for(var i = 0; i < number*2; i++){
+          allArray.push(cssArray[i]);
+          allArray.push(maxIntArray[i]);
         }
-        if(n != null){
-          for(var l = 0; l < cool.length; l++){
-            var stringToReplace = maxInt.toString() + "px";
-            var re = new RegExp(stringToReplace, 'g');
-            var replacedString = cool.replace(re, l.toString() + "px");
-            console.log(replacedString);
-          }
-        }
-        cssArray.join(" ");
+        console.log(allArray)
     };
-  var css = createCss(100, 10, 60);
-  var newCssString = cssArray.join(" ");
+  var css = createCss(60, 10);
+  var newCssString = allArray.join("");
