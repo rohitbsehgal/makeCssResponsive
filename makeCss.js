2c30142c853c94f9f9302b49d95e47aff8e57a2a
Rohit Sehgal <Rohit@varuns-iphone.local>
2015-03-24 22:16:05 -0500

    update

--- makeCss.js
+++ makeCss.js
@@ -1,49 +1,63 @@
 var cssArray = [];
 var allArray = [];
-var createCss = function(number, increment, numberToReplace){
-    var x = 0;
-    var x1 = 0;
+var numToReplace = 0;
+// testing: { .h1{ font-size: 60px; } }
+var lineToCreateCss = function(line){
+  var n = 0;
+  var y = 0;
+  var x = "";
+  for(var j = 0; j < line.length; j++){
+    n = line.indexOf("px");
+    y = line.indexOf(":");
+  }
+  if(n != null){
+    for(var i = y; i < n+1; i++){
+      if (line.charAt(i) >= '0' && line.charAt(i) <= '9') {
+        var charToConcat = line.charAt(i);
+        var x = x.concat(charToConcat);
+        console.log(line.charAt(i));
+        console.log(x);
+      }
+    }
+    return x;
+  }else{
+    return "Error";
+  }
+};
+var createCss = function(number, increment, numberToReplace, percentOfScreen, lineOfCss){
+     var x = 0;
+     var x1 = 0;
      var n = 0;
      var z = 0;
      var stringToReplaceNumber = 0;
      var stringToReplace = ""
      var maxIntArray = [];
-
-     if(number < 10){
-       maxIntDecimal = 0.1;
-     }else if(number > 99 && number > 999){
-       maxIntDecimal = 0.01;
-     }else if(number > 999 && number > 9999){
-       maxIntDecimal = 0.001;
-     }else if(number > 9999 && number > 99999){
-       maxIntDecimal = 0.0001;
-     }else if(number > 99999 && number > 999999){
-       maxIntDecimal = 0.00001;
-     }else if(number > 999999 && number > 9999999){
-       maxIntDecimal = 0.000001;
-     }
+     var ems = (number*increment)/16;
+     var percent = ems*0.01*percentOfScreen;
+     var pixels = number*increment;
+     var subtractBy = (ems*percentOfScreen*0.01) - ((pixels - increment)/16)*percentOfScreen*0.01;
      for(var k = 0; k < number; k++){
        x1 += increment;
      }
-     var cool = "{ .h1{ font-size: 60px; } } "
+     var cool = lineOfCss;
      for(var j = 0; j < cool.length; j++){
        n = cool.indexOf("px");
      }
        if(n != null){
-         for(var l = 0; l < number; l++){
+         for(var l = number; l > 0; l--){
            console.log(l.toString())
            stringToReplaceNumber = number - l;
            console.log(stringToReplaceNumber)
            stringToReplace = numberToReplace.toString() + "px";
            var re = new RegExp(stringToReplace, 'g');
-           var replacedString = cool.replace(re, (l/15).toString() + "em");
+           var replacedString = cool.replace(re, percent-subtractBy*l + "em");
            console.log(replacedString);
-           maxIntArray.push(replacedString)
+           maxIntArray.push(replacedString + " }")
          }
        }
         for(var i = 0; i < number; i++){
             x+= increment;
-            cool = "@media(min-width: " + x + "px)";
+            cool = "@media(min-width: " + x + "px){ ";
             cssArray.push(cool);
 
         };
@@ -52,7 +66,15 @@ var createCss = function(number, increment, numberToReplace){
           allArray.push(cssArray[i]);
           allArray.push(maxIntArray[i]);
         }
-        console.log(allArray)
-    };
-  var css = createCss(60, 10, 60);
-  var newCssString = allArray.join("");
+        console.log(allArray);
+        console.log(subtractBy);
+        return allArray.join("");
+
+};
+var doItAll = function(number, increment, percentOfScreen, lineOfCss){
+  var numToReplace = lineToCreateCss(lineOfCss);
+  var css = createCss(number, increment, numToReplace, percentOfScreen, lineOfCss);
+  return css;
+}
+  var doFunctions = doItAll(60, 25, 5,".h1{ font-size: 60px; }");
+  console.log(doFunctions);
